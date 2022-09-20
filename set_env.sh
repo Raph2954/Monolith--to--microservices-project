4d366dbfa83834146ab01b28d6fdd6af4b1ea01c
Raphael Uchechukwu  Daniel <raph.man8@gmail.com>
2022-09-20 16:43:34 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.cycczidwkpmw.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udagramraph01
+export POSTGRES_HOST=database-1.cakp7q7ljryh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=database-1
+export AWS_BUCKET=arn:aws:s3:::udagram-001
 export AWS_REGION=us-east-1a
 export AWS_PROFILE=default
 export JWT_SECRET=testing
